9d11e9709e2468ad2b0c6eda913c1fed617ea108
Jason Vennin <jason.vennin@gmail.com>
2021-06-03 13:57:34 +0200

    build(initial): initialing the git

---
+++ src/main/java/com/parcs/ParcsApplication.java
@@ -0,0 +1,13 @@
+package com.parcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParcsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/parcs/models/Visiteur.java
@@ -0,0 +1,30 @@
+package com.parcs.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+public class Visiteur {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name = "nom")
+	private String nom;
+	
+	@Column(name = "prenom")
+	private String prenom;
+	
+	@Column(name = "age")
+	private int age;
+
+}
